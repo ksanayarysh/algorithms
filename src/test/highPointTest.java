85080b1f26db38057d8af4a782d19380c8b12040
KY <ksana32@gmail.com>
2019-02-10 23:28:26 +0500

    реструктуризация

---
+++ src/test/highPointTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class highPointTest {
+
+    @Test
+    public void countHigh() {
+    }
+
+    @Test
+    public void test1(){
+        int a = 9;
+        int b = 3;
+        int expectedResult = 3;
+        Assert.assertEquals(expectedResult, a / b);
+    }
+}
